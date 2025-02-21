391ef60dac5fbef2a0c9c1b7d292e5e04c98c4ff
Anshika Saini <110725511+AnshikkaSaini@users.noreply.github.com>
2025-02-21 14:23:03 +0000

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,4 @@
+// Example JavaScript (e.g., show an alert on button click)
+document.querySelector('button').addEventListener('click', function() {
+    alert('You clicked the "View Project" button!');
+});
