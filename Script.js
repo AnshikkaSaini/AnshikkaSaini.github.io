bf3f571a1474ae9c92702266022182e42fd68275
Anshika Saini <110725511+AnshikkaSaini@users.noreply.github.com>
2025-02-21 17:10:20 +0000

    Delete Script.js

--- Script.js
+++
@@ -1,4 +0,0 @@
-// Example JavaScript (e.g., show an alert on button click)
-document.querySelector('button').addEventListener('click', function() {
-    alert('You clicked the "View Project" button!');
-});
